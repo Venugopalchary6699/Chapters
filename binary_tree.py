e00c445d9158cd240985f821fb17b8450fc5505b
venugopalchary6699 <rvgcrdc@gmail.com>
2022-07-24 12:38:23 +0530

    search functio

--- binary_tree.py
+++ binary_tree.py
@@ -4,13 +4,14 @@ class binarysearchtree:
         self.left = None
         self.right = None
 
-    def add_child(self,data):
+    def add_child(self, data):
         if data == self.data:
             return
+
         if data < self.data:
             # add data in left subtree
-            if self.data:
-                self.data.add_child(data)
+            if self.left:
+                self.left.add_child(data)
             else:
                 self.left = binarysearchtree(data)
         else:
@@ -18,22 +19,39 @@ class binarysearchtree:
                 self.right.add_child(data)
             else:
                 self.right = binarysearchtree(data)
-        def in_order_traversal(self):
-            elements = []
-            #visit left tree
+
+    def in_order_traversal(self):
+        elements = []
+
+        # visit left tree
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        # Visit base node
+        elements.append(self.data)
+
+        # Visit right tree
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+    def search(self,val):
+        if val == self.data:
+            return True
+        if val < self.data:
             if self.left:
-                elements += self.left.in_order_traversal()
-            # Visit base node
-            elements.append(self.data)
 
-            # Visit right tree
+                return self.left.search(val)
+            else:
+                return False
+        if val > self.data:
             if self.right:
-                elements += self.right.in_order_traversal()
-
-            return elements
+                return self.right.search(val)
+            else:
+                return False
 
 def build_tree(elements):
-    root = binarysearchtree(element[0])
+    root = binarysearchtree(elements[0])
 
     for i in range(1, len(elements)):
         root.add_child(elements[i])
@@ -41,11 +59,13 @@ def build_tree(elements):
 
 
 
-
-if __name__ == '__,main__':
-    numbers = [17, 4, 1, 20, 9, 23, 18, 34]
+if __name__ == '__main__':
+    numbers = [17, 4, 1, 20, 9, 23, 18,18 , 34]
     numbers_tree = build_tree(numbers)
-    print(numbers_tree)
+    countries = ["India", "Pakisthan", "US0", "canada", "western"]
+    countrees = build_tree(countries)
 
-
-            #Visit right tree
+    print(numbers_tree.in_order_traversal())
+    print(countrees.in_order_traversal())
+    print(numbers_tree.search(18))
+    print(countrees.search("India"))
